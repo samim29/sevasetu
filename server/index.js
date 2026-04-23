0985dd8e94deedc2e7d007ab1de2a8ea4d6c47ec
Samim29 <samimalisk000@gmail.com>
2026-04-23 11:01:11 +0530

    Updated the index,js file for deployment

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv')
-
+const path = require('path')
 dotenv.config()
 
 require('./models/User')
@@ -39,4 +39,5 @@ app.get('/', (req, res) => {
 
 
 const PORT = process.env.PORT || 5000
+app.use(express.static(path.join(__dirname, '../client/dist')))
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
